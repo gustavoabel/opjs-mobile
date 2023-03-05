8a2c809da64a80ded701a4726ca06391cbb1dc7a
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-05 14:54:38 -0400

    style: adjust product details margin

--- src/components/Menu/styles.tsx
+++ src/components/Menu/styles.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 
-export const Product = styled.View``;
+export const Product = styled.View`
+flex-direction: row`;
 
 export const ProductImage = styled.Image`
 width: 120px;
@@ -8,4 +9,7 @@ height: 96px;
 border-radius: 8px;
 `;
 
-export const ProductDetails = styled.View``;
+export const ProductDetails = styled.View`
+margin-left: 16px;
+flex: 1;
+`;
