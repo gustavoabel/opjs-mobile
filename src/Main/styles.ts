59facccfc5ad4d8c3014a79e8039b29f5c51a137
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-05 12:15:39 -0400

    feat: create button component and footer

--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -22,5 +22,8 @@ flex: 1;
 export const Footer = styled.View`
 min-height: 110px;
 background: #fff;
+padding: 16px 24px;
 `;
-export const FooterContainer = styled.SafeAreaView``;
+
+export const FooterContainer = styled.SafeAreaView`
+`;
