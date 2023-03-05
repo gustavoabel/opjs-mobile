7bcf4ae84a16f47fa9b89e9ec660edf9c13a73e6
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-05 14:51:58 -0400

    feat: create menu component

---
+++ src/components/Menu/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Product = styled.View``;
+
+export const ProductImage = styled.Image`
+width: 120px;
+height: 96px;
+border-radius: 8px;
+`;
+
+export const ProductDetails = styled.View``;
