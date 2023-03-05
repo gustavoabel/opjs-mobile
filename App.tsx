cf06f51137bde0d5d99181f310f05ceaf6b66bbf
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-04 23:00:10 -0400

    feat: add header component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
 import { Main } from './src/Main';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
     'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
-    'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
-    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
   });
 
   if (!isFontsLoaded) {
@@ -13,7 +14,10 @@ export default function App() {
   }
 
   return (
-    <Main />
+    <>
+      <StatusBar style='dark' />
+      <Main />
+    </>
   );
 }
 
