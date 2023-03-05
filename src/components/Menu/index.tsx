a7e44500803fd182264eb400f2b7707a7d24e943
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-05 15:08:30 -0400

    build: install intl and intl-ptBR

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import { FlatList } from 'react-native';
 import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurrency';
 import { Text } from '../Text';
 
 import { Product, ProductImage, ProductDetails } from './styles';
@@ -22,8 +23,7 @@ export function Menu() {
             <Text weight='600'>{product.name}</Text>
             <Text size={14} color="#666" style={{ marginVertical: 8 }}>
               {product.description}</Text>
-            <Text size={14} weight="600">
-              {product.price}</Text>
+            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
           </ProductDetails>
         </Product>
       )}
