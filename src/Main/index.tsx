1a734455912971d837a1d94a278efbf9fa178006
gustavoabel <contato.gustavoabel@gmail.com>
2023-03-11 22:08:26 -0300

    feat: finish Cart component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -14,11 +14,21 @@ import { TableModal } from '../components/TableModal';
 import { useState } from 'react';
 import { Cart } from '../components/Cart';
 import { CartItem } from '../types/CartItem';
+import { products } from '../mocks/products';
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([
+    {
+      quantity: 1,
+      product: products[0],
+    },
+    {
+      quantity: 1,
+      product: products[1],
+    }
+  ]);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,14 +1,66 @@
-import { FlatList } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native";
+
 import { CartItem } from "../../types/CartItem";
+
+import {
+  Item,
+  ProductContainer,
+  Actions,
+  Image,
+  QuantitiyContainer,
+  ProductDetails
+} from './styles';
+
+import { Text } from '../Text';
+import { formatCurrency } from "../../utils/formatCurrency";
+import { PlusCircle } from "../Icons/PlusCircle";
+import { MinusCircle } from "../Icons/MinusCircle";
+
 
 interface CartProps {
   cartItems: CartItem[];
 }
 
-export function Cart({ cartItems: [] }: CartProps) {
+export function Cart({ cartItems }: CartProps) {
   return (
     <FlatList
-    data={[]}
+    data={cartItems}
+    keyExtractor={cartItem => cartItem.product._id}
+    showsVerticalScrollIndicator={false}
+    renderItem={({ item: cartItem }) => (
+      <Item>
+        <ProductContainer>
+          <Image
+          source={{
+            uri: `http://192.168.0.28:3001/uploads/${cartItem.product.imagePath}`,
+          }}
+          />
+
+          <QuantitiyContainer>
+            <Text size={14} color="#666">
+              {cartItem.quantity}x
+            </Text>
+          </QuantitiyContainer>
+
+          <ProductDetails>
+            <Text size={14} weight="600">{cartItem.product.name}</Text>
+            <Text size={14} color="#666" style={{ marginTop: 4}}>
+              {formatCurrency(cartItem.product.price)}
+              </Text>
+          </ProductDetails>
+        </ProductContainer>
+
+        <Actions>
+          <TouchableOpacity style={{marginRight: 24}}>
+            <PlusCircle></PlusCircle>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <MinusCircle></MinusCircle>
+          </TouchableOpacity>
+        </Actions>
+      </Item>
+    )}
     />
-  )
+  );
 }
